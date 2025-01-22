7d5f64fdae57fd745721d9eab7636bf6ad90ab7b
David C. O'Gara <davidogara@hotmail.co.uk>
2025-01-22 17:21:57 +0000

    Add initial templates and set up css file

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["SECRET_KEY"] = "#[S+jN2)bTJ4Q.oKzjL)+_xJZSQvFY"
--- triviasurvivor/settings.py
+++ triviasurvivor/settings.py
@@ -12,15 +12,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2w1^sufzh25l69bcjag)6l(71#gb3n!0vuem5)7jp)gzl=kt*b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Fallback in case SECRET_KEY is not set
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not set in environment or env.py")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,7 +91,7 @@ ROOT_URLCONF = 'triviasurvivor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
