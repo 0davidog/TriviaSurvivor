1ebc59c1a001ce47bb10749638b49781b3ee9c33
David C. O'Gara <davidogara@hotmail.co.uk>
2025-02-03 18:50:14 +0000

    Add links to and style authentication templates

--- static/js/game.js
+++ static/js/game.js
@@ -1,17 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const titleMenu = document.getElementById("title-menu");
+    const homeBtn = document.getElementById("home-btn");
+    const topBtn = document.getElementById("top-btn");
     const playBtn = document.getElementById("play-btn");
     const playCard = document.getElementById("play-card");
     const guestBtn = document.getElementById("guest-btn");
     const guestPlayCard = document.getElementById("guest-play-card");
 
+    if(topBtn) {
+        topBtn.focus();
+    }
+
+    if(homeBtn) {
+        window.addEventListener("keydown", function (e) {
+            if (e.key === "Home") {
+                window.location.href = "/"; // Redirect to the home page
+            }
+        });}
+
     if (playBtn) {
         playBtn.addEventListener("click", function () {
             titleMenu.classList.add("hidden");
             playCard.classList.remove("hidden");
-           
-            
     });}
 
     if (guestBtn) {
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,28 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const modal = document.getElementById("custom-modal");
-    const openBtn = document.getElementById("menu-btn");
+    const menuModal = document.getElementById("menu-modal");
+    const profileModal = document.getElementById("profile-modal");
+    const infoModal = document.getElementById("info-modal");
+    const menuBtn = document.getElementById("menu-btn");
     const closeBtn = document.getElementById("close-btn");
 
     // Open modal
-    openBtn.addEventListener("click", function () {
-        modal.style.display = "flex";
-        openBtn.style.display = "none";
+    menuBtn.addEventListener("click", function () {
+        menuModal.style.display = "flex";
+        menuBtn.style.display = "none";
         closeBtn.style.display = "block";
+        if (profileModal) {
+            profileModal.style.display = "none";
+        }
     });
 
     // Close modal
     closeBtn.addEventListener("click", function () {
-        modal.style.display = "none";
+        menuModal.style.display = "none";
         closeBtn.style.display = "none";
-        openBtn.style.display = "block";
+        menuBtn.style.display = "block";
+    });
+
+    // Close modal if user presses esc key
+    window.addEventListener("keydown", function (e) {
+        if (e.key === "Escape") {
+            if (menuModal.style.display === "flex") {
+                menuModal.style.display = "none";
+                closeBtn.style.display = "none";
+                menuBtn.style.display = "block";
+            } else if (profileModal.style.display === "flex") {
+                profileModal.style.display = "none";
+                closeBtn.style.display = "none";
+                menuBtn.style.display = "block";
+            } else if (infoModal.style.display === "flex") {
+                infoModal.style.display = "none";
+                closeBtn.style.display = "none";
+                menuBtn.style.display = "block";
+            } else {
+                menuModal.style.display = "flex";
+                menuBtn.style.display = "none";
+                closeBtn.style.display = "block";
+            }   
+        }
     });
 
     // Close modal if user clicks outside the modal content
     window.addEventListener("click", function (event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
+        if (event.target === menuModal) {
+            menuModal.style.display = "none";
             closeBtn.style.display = "none";
-            openBtn.style.display = "block";
+            menuBtn.style.display = "block";
         }
     });
+ 
 });
