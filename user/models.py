a81c4345475df5d71acae593ec8c98d961fd5cc0
David C. O'Gara <davidogara@hotmail.co.uk>
2025-05-01 18:21:54 +0100

    Add additional fields to Userdata model

--- user/models.py
+++ user/models.py
@@ -9,8 +9,18 @@ class UserData(models.Model):
     """
     Model to represent saved player data and profile information.
     """
+    icon_choices = [
+        ('01', 'icon01'),
+        ('02', 'icon01'),
+        ('03', 'icon01'),
+        ('04', 'icon01'),
+        ('05', 'icon01'),
+        ('06', 'icon01'),
+        ('07', 'icon01')]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=True)
+    profile_icon = models.CharField(choices=icon_choices, default='04')
     nationality = CountryField(blank_label="(select country)", blank=True)
 
     def __str__(self):
