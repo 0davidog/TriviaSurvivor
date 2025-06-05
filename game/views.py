3e7fb05267b2f5ef27de3e7b9e9172df3d4aac71
David C. O'Gara <davidogara@hotmail.co.uk>
2025-06-05 11:59:44 +0100

    Add template for quiz layout

--- game/views.py
+++ game/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from user.models import UserData
 
-# Create your views here.
-
 def index(request):
     """
     A view to return the index page.
     """
 
-    user_data = get_object_or_404(UserData, user=request.user)
+    user_data = None
+    icon_url = None
 
-    icon_url = f"images/player_icons/icon{user_data.player_icon}.png"
+    if request.user.is_authenticated:
+        try:
+            user_data = UserData.objects.get(user=request.user)
+            icon_url = f"images/player_icons/icon{user_data.player_icon}.png"
+        except UserData.DoesNotExist:
+            user_data = None
+            icon_url = None
 
     context = {
-        'user_data' : user_data,
-        'icon_url' : icon_url,
+        'user_data': user_data,
+        'icon_url': icon_url,
     }
-    
-    # Render the index page with the provided context
+
     return render(request, 'game/index.html', context)
