c6a5c4841132b86c8cd8f27873d75ae9339a3bfe
David C. O'Gara <davidogara@hotmail.co.uk>
2025-09-05 15:29:34 +0100

    Add view_profile views urls and html

--- user/urls.py
+++ user/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import view_profile
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('game.urls')),
-    path('workshed/', include('workshed.urls')),
-    path('user/', include('user.urls')),
+    path('', view_profile, name='view_profile'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserData
 
 # Create your views here.
+
+@user_passes_test(lambda u: u.is_authenticated)
+def view_profile(request):
+
+    user_data = get_object_or_404(UserData, user=request.user)
+
+    context = {
+        'user_data': user_data,
+    }
+
+    return render(request, 'user/view_profile.html', context)
