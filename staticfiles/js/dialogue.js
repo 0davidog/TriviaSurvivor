4b53b7e953862088a238d140d37040fef63b4467
David C. O'Gara <davidogara@hotmail.co.uk>
2026-02-13 15:29:36 +0000

    Feat: Add carousel to genre select

--- staticfiles/js/dialogue.js
+++ staticfiles/js/dialogue.js
@@ -3,31 +3,31 @@
 export const dialogueSet = {
     title: {
         header: "Welcome",
-        text: `Trivia Survivor is a movie trivia quiz game focused on the horror genre. <br>Answer questions correctly on the genre of your choice to survive the nightmare.`
+        text: `This is Trivia Survivor, test your knowledge of horror movie trivia against an approaching danger. Your brain is your only defence. Answer enough questions correctly to survive the nightmare. Good luck.`
     },
     intro_1: {
-        header: "Intro 1",
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, autem pariatur sit ratione rem quis magni porro quae inventore, deserunt delectus, alias incidunt voluptatem quibusdam harum exercitationem esse suscipit placeat.`
+        header: "Introduction",
+        text: `“...this is an emergency broadcast. We have received wide-spread reports of horror movie villains escaping the screen to attack victims...”`
     },
     intro_2: {
-        header: "Intro 2",
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, autem pariatur sit ratione rem quis magni porro quae inventore, deserunt delectus, alias incidunt voluptatem quibusdam harum exercitationem esse suscipit placeat.`
+        header: "Introduction",
+        text: `“...these creatures seem to be most attracted to those who are ignorant of the world they came from. We advise all citizens to remain indoors and watch only romantic comedies...”`
     },
     intro_3: {
-        header: "Intro 3",
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, autem pariatur sit ratione rem quis magni porro quae inventore, deserunt delectus, alias incidunt voluptatem quibusdam harum exercitationem esse suscipit placeat.`
+        header: "Introduction",
+        text: `“...if caught, your only hope is to repel these creatures with knowledge and escape.”`
     },
     chapter_zombie: {
         header: `Chapter 1: The Zombie`,
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, autem pariatur sit ratione rem quis magni porro quae inventore, deserunt delectus, alias incidunt voluptatem quibusdam harum exercitationem esse suscipit placeat.`
+        text: `Exploring the abandoned mansion seemed like a great idea for video content at the time, but the various scattered lab reports and pieces of medical equipment suggest a much darker mystery best left alone. You try to leave, but in the darkness of the corridor that is your only way out you hear the shuffling steps and anguished moans of something rotten and very, very hungry...`
     },
     chapter_psycho: {
         header: `Chapter 2: The Psycho`,
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, autem pariatur sit ratione rem quis magni porro quae inventore, deserunt delectus, alias incidunt voluptatem quibusdam harum exercitationem esse suscipit placeat.`
+        text: `Work was miserable, now it's late and all you want to do is eat pizza, shower and bed, and not necessarily in that order. The whole way home you felt like someone was watching you and now, as your stress-fried brain causes you to drop the keys to your flat, you feel a now overwhelming bloodlust from the darkness behind you. You turn around as something cold begins to approach...`
     },
     chapter_onryō: {
         header: `Chapter 3: The Onryō`,
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, autem pariatur sit ratione rem quis magni porro quae inventore, deserunt delectus, alias incidunt voluptatem quibusdam harum exercitationem esse suscipit placeat.`
+        text: `You missed the last train but fortified by beer and grilled snacks you decide to walk home. As you get close to your street, you notice something doesn’t feel right. A noise like radio static begins to build in your head, maybe it’s just the beers from earlier, or maybe the barrier between here and the spirit world has become thin and something malevolent and cursed comes this way...`
     },
     correct_1: {
         header: `Correct - 1 Point`,
